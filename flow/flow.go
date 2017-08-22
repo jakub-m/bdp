bf80898a9699f2aba3cab407c287bd51912fd74d
Jakub Mikians <jakub.mikians@gmail.com>
2017-08-22 17:10:53 +0200

    Print rtt

--- flow/flow.go
+++ flow/flow.go
@@ -10,19 +10,7 @@ func ProcessFrames(frames []*frames.Frame) error {
 	flow := &flow{}
 
 	for _, f := range frames {
-		if f.TCP.IsSyn() && !f.TCP.IsAck() { // syn
-			flow.local.ip = f.IP.SourceIP()
-			flow.local.port = f.TCP.SourcePort()
-			flow.local.initSeqNum = f.TCP.SeqNum()
-			flow.remote.ip = f.IP.DestIP()
-			flow.remote.port = f.TCP.DestPort()
-			flow.initTimestamp = f.Record.Timestamp()
-		}
-
-		if f.TCP.IsSyn() && f.TCP.IsAck() { // syn ack
-			flow.remote.initSeqNum = f.TCP.SeqNum()
-		}
-
+		flow.consumeFrame(f)
 		msg := flow.fmtFrame(f)
 		fmt.Println(msg)
 	}
@@ -32,10 +20,12 @@ func ProcessFrames(frames []*frames.Frame) error {
 // initTimestamp initial timestamp in microseconds
 // local is the side that initiates connection (syn).
 // remote is the other side of the connection (syn ack).
+// inflight are the files that are sent from local to remote and are not yet acknowledged.
 type flow struct {
 	initTimestamp uint64
 	local         flowDetails
 	remote        flowDetails
+	inflight      []*frames.Frame
 }
 
 // initSeqNum is initial sequence number.
@@ -43,6 +33,73 @@ type flowDetails struct {
 	ip         pcap.IPv4
 	port       uint16
 	initSeqNum pcap.SeqNum
+}
+
+func (f *flow) consumeFrame(frame *frames.Frame) {
+	if frame.TCP.IsSyn() && !frame.TCP.IsAck() {
+		f.onSyn(frame)
+	} else if frame.TCP.IsSyn() && frame.TCP.IsAck() {
+		f.onSynAck(frame)
+	} else if f.isLocalToRemote(frame) {
+		f.onSend(frame)
+	} else if f.isRemoteToLocal(frame) && frame.TCP.IsAck() {
+		f.onAck(frame)
+	}
+}
+
+func (f *flow) onSyn(frame *frames.Frame) {
+	f.local.ip = frame.IP.SourceIP()
+	f.local.port = frame.TCP.SourcePort()
+	f.local.initSeqNum = frame.TCP.SeqNum()
+	f.remote.ip = frame.IP.DestIP()
+	f.remote.port = frame.TCP.DestPort()
+	f.initTimestamp = frame.Record.Timestamp()
+}
+
+func (f *flow) onSynAck(frame *frames.Frame) {
+	f.remote.initSeqNum = frame.TCP.SeqNum()
+}
+
+// Packets sent are inflight until acknowledged. Only packets with payload are expected to be acknowledged (i.e. pure 'acks' with no payload do not count as inflight.)
+func (f *flow) onSend(frame *frames.Frame) {
+	payloadSize := frame.PayloadSize()
+	if payloadSize == 0 {
+		return
+	}
+	f.inflight = append(f.inflight, frame)
+	fmt.Print("inflight ", len(f.inflight))
+	for _, g := range f.inflight {
+		fmt.Print(" ", f.getExpectedSeqNum(g))
+	}
+	fmt.Println()
+}
+
+func (f *flow) onAck(frame *frames.Frame) {
+	ackNum := frame.TCP.AckNum().RelativeTo(f.local.initSeqNum)
+	for i, g := range f.inflight {
+		// Consider adding "flowFrame" wrapping frame and flow-related information (is local, expected seq num).
+		expSeqNum := f.getExpectedSeqNum(g)
+		if ackNum == expSeqNum {
+			f.inflight = f.inflight[i+1:]
+			// what if there are many packets acknowledged?
+			fmt.Println("got ack match: ", i, ackNum, "rtt:", frame.Record.Timestamp()-g.Record.Timestamp(), "us")
+		}
+	}
+}
+
+func (f *flow) getExpectedSeqNum(frame *frames.Frame) uint32 {
+	payloadSize := uint32(frame.PayloadSize())
+	if f.isLocalToRemote(frame) {
+		// FIXME: handle next syn at integer boundaries gracefully.
+		relSyn := frame.TCP.SeqNum().RelativeTo(f.local.initSeqNum)
+		return relSyn + payloadSize
+	}
+	if f.isRemoteToLocal(frame) {
+		relSyn := frame.TCP.SeqNum().RelativeTo(f.remote.initSeqNum)
+		return relSyn + payloadSize
+	}
+	// This should never happen since all the frames for a flow should be either local-to-remote or remote-to-local.
+	panic(fmt.Sprintf("getExpectedSeqNum invoked for frame that does not belong to the flow. flow: %v, frame: %v", f, frame))
 }
 
 func (f *flow) fmtFrame(frame *frames.Frame) string {
